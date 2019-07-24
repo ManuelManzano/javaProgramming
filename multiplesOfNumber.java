4b67eb525328d13458e19fcee258cc0a31045852
Manuel Manzano <manuelalejandromanzano@gmail.com>
2019-07-24 14:54:34 -0400

    Create multiplesOfNumber.java

---
+++ multiplesOfNumber.java
@@ -0,0 +1,15 @@
+public class multiples {
+
+	public static void main(String[] args) {
+
+		int number = 1;
+
+		while (number <= 125) {
+			number = number * 7;
+			System.out.println(number);
+
+		}
+
+	}
+
+}
